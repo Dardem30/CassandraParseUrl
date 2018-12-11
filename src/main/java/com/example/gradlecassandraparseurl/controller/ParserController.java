9b9bad9b09950ab370224b061fd4aea3f8a204b7
Roman <rlukashenko@cygno.com>
2018-12-11 19:04:23 +0300

    without cassandra to heroku (with love)

--- src/main/java/com/example/gradlecassandraparseurl/controller/ParserController.java
+++ src/main/java/com/example/gradlecassandraparseurl/controller/ParserController.java
@@ -1,12 +1,10 @@
 package com.example.gradlecassandraparseurl.controller;
 
+import com.example.gradlecassandraparseurl.controller.dto.RequestForm;
 import com.example.gradlecassandraparseurl.service.ParseUrlService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,15 +16,15 @@ public class ParserController {
     private final ParseUrlService parseUrlService;
 
     @PostMapping(value = "/parseUrlForAdvertisements")
-    public final ResponseEntity<List<String>> parseUrlForAdvertisements(final @RequestParam("url") String url) throws IOException {
-        return ResponseEntity.ok(parseUrlService.parseForAdvertisements(url));
+    public final ResponseEntity<List<String>> parseUrlForAdvertisements(final @RequestBody RequestForm url) throws IOException {
+        return ResponseEntity.ok(parseUrlService.parseForAdvertisements(url.getUrl()));
     }
     @PostMapping(value = "/typeOfLinks")
-    public final ResponseEntity typeOfLinks(final @RequestParam("url") String url) throws IOException {
-        return ResponseEntity.ok(parseUrlService.parseForType(url));
+    public final ResponseEntity typeOfLinks(final @RequestBody RequestForm url) {
+        return ResponseEntity.ok(parseUrlService.parseForType(url.getUrl()));
     }
     @PostMapping(value = "/isImage")
-    public final ResponseEntity isImage(final @RequestParam("url") String url) throws IOException {
-        return ResponseEntity.ok(parseUrlService.isImage(url));
+    public final ResponseEntity isImage(final @RequestBody RequestForm url) throws IOException {
+        return ResponseEntity.ok(parseUrlService.isImage(url.getUrl()));
     }
 }
---
+++ src/main/java/com/example/gradlecassandraparseurl/controller/dto/RequestForm.java
@@ -0,0 +1,13 @@
+package com.example.gradlecassandraparseurl.controller.dto;
+
+public class RequestForm {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
