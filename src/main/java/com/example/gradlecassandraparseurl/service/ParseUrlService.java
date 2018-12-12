794ddfd8581cf1b4dc4b9cd83308cb57322b41bd
Roman <rlukashenko@cygno.com>
2018-12-12 17:01:43 +0300

    need refactoring

--- src/main/java/com/example/gradlecassandraparseurl/service/ParseUrlService.java
+++ src/main/java/com/example/gradlecassandraparseurl/service/ParseUrlService.java
@@ -16,6 +16,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,8 +27,10 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ParseUrlService {
     private final LinkDAO linkDAO;
+
     /**
      * This method get all advertisements href of page
+     *
      * @param url input url
      * @return string(all url of advertisements)
      */
@@ -46,8 +49,10 @@ public class ParseUrlService {
             throw new IOException(e);
         }
     }
+
     /**
      * This method get all href(with type) of page
+     *
      * @param url input url
      * @return string(all url of advertisements)
      */
@@ -56,7 +61,7 @@ public class ParseUrlService {
             final Map<String, List<String>> result = new HashMap<>();
             final Document doc = Jsoup.connect(url).get();
             final Elements links = doc.select("a[href]");
-            result.put("images",  links.stream()
+            result.put("images", links.stream()
                     .filter(link -> link.children().stream().anyMatch(chil -> chil.tag().getName().equals("img")))
                     .map(link -> link.attr("href"))
                     .collect(Collectors.toList()));
@@ -70,7 +75,8 @@ public class ParseUrlService {
             throw new IOException(e);
         }
     }
-    public final Map<String, String> getAllAdvertisementsLinkedImages(final String url) throws IOException {
+
+    public final Map<String, ArrayList<String>> getAllAdvertisementsLinkedImages(final String url) throws IOException {
         try {
             final Document doc = Jsoup.connect(url).get();
             final Elements links = doc.select("a");
@@ -79,20 +85,25 @@ public class ParseUrlService {
                     .filter(link -> link.children().stream().anyMatch(chil -> chil.tag().getName().equals("img")) && link.attr("href").contains("http") && !host.equals(URI.create(link.attr("href")).getHost()))
                     .filter(link -> link.children().attr("src").contains("http"))
                     .collect(Collectors.toList());
-            Map<String,String> result = new HashMap<>();
+            Map<String, String> result = new HashMap<>();
             linked.forEach(link -> result.put(link.attr("href"), link.children().attr("src")));
-            return result;
+            Map<String, ArrayList<String>> res = new HashMap<>();
+            res.put("links", new ArrayList<>(result.keySet()));
+            res.put("images", new ArrayList<>(result.values()));/////////// Я БУДУ РЕФАКТОРИТЬ!!!!!!!
+            return res;
         } catch (final IOException e) {
             log.error("Error during parsing url for advertisements", e);
             throw new IOException(e);
         }
     }
+
     public final boolean isImage(final String url) throws IOException {
         final URL imUrl = new URL(url);
         final BufferedImage bufferedImage = ImageIO.read(imUrl);
         return bufferedImage != null && new ImageIcon(bufferedImage).getImage().getWidth(null) != -1;
     }
-    public final Iterable<Link> test(){
+
+    public final Iterable<Link> test() {
         return linkDAO.findAll();
     }
 }
